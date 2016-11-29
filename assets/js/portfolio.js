117fa09e3ba22a1a006acd59b22ac9d7770a62eb
okkim21 <okkim@Oks-MacBook-Pro.home>
2016-11-28 22:53:12 -0800

    add more portfolio

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,19 +1,27 @@
 (function($){
 
     var tl = new TimelineLite(),
+        name = document.getElementById("name"),
         greetingText = document.getElementById("greeting-text"),
         content01 = document.getElementById("content01-01"),
+        content0102 = document.getElementById("content01-02"),
         content02 = document.getElementById("content02-01"),
-        contentDivider = document.getElementById("content-divider");
+        content0202 = document.getElementById("content02-02");
 
+
+    TweenLite.set(name, {autoAlpha:0});
     TweenLite.set(greetingText, {autoAlpha:0, rotationX:-90});
     TweenLite.set(content01, {autoAlpha:0, rotationX:-90});
-    TweenLite.set(contentDivider, {autoAlpha:0, rotationX:90});
+    TweenLite.set(content0102, {autoAlpha:0, rotationX:-90});
     TweenLite.set(content02, {autoAlpha:0, rotationX:-90});
+    TweenLite.set(content0202, {autoAlpha:0, rotationX:-90});
 
-    tl.to(greetingText, 0.7, {autoAlpha:1,rotationX:0, transformOrigin:"50% 50% -25px"})
-      .to(content01, 0.7, {autoAlpha:1,rotationX:0, transformOrigin:"50% 50% -25px"})
-      .to(contentDivider, 0.7,{autoAlpha:.3,rotationX:0, transformOrigin:"0% 0% -25px"})
-      .to(content02, 0.7, {autoAlpha:1,rotationX:0, transformOrigin:"50% 50% -25px"});
+    tl
+        .to(name, 0.7, {autoAlpha:1, ease: Power1.easeIn})
+        .to(greetingText, 0.7, {autoAlpha:1,rotationX:0, transformOrigin:"50% 50% -25px"})
+        .to(content01, 0.7, {autoAlpha:1,rotationX:0, transformOrigin:"50% 50% -25px"})
+        .to(content0102, 0.7, {autoAlpha:1,rotationX:0, transformOrigin:"50% 50% -25px"})
+        .to(content02, 0.7, {autoAlpha:1,rotationX:0, transformOrigin:"50% 50% -25px"})
+        .to(content0202, 0.7, {autoAlpha:1,rotationX:0, transformOrigin:"50% 50% -25px"});
 
 }(jQuery));
